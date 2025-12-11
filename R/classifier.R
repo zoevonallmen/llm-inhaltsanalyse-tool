0c28b224fd643bbbc895a88955ca766206159eb8
zoevonallmen <z.vonallmen@gmail.com>
2025-12-11 13:58:10 +0100

    minor adjustments

--- R/classifier.R
+++ R/classifier.R
@@ -1,19 +1,24 @@
-#Initialize model -----------------------------------------------------------------------
+# Load Libraries ---------------------------------------------------------------
 library(ellmer)
 library(stringr)
 library(jsonlite)
 
-
+# Initialize Model -------------------------------------------------------------
 LLAMA_MODEL <- "meta-llama/Llama-3.1-8B-Instruct"
 API_KEY <- Sys.getenv("HUGGINGFACE_API_KEY")
 
 hf_model <- ellmer::chat_huggingface(
   system_prompt = "Du bist der Classifier. Deine Aufgabe ist die strikte Anwendung des Codierschemas. Antworte IMMER im JSON-Format mit den Feldern 'code' und 'reasoning'.",  #system-prompt noch überarbeiten/begründe; und DE or EN?
   model = LLAMA_MODEL,
-  credentials = function() API_KEY
+  credentials = function() API_KEY,
+  params = list(
+    temperature = 0.0,#?
+    max_new_tokens = 300 #?
+  ),
+  echo = "none"
 )
 
-# Classifier  ----------------------------------------------------------------
+# Classifier Function ----------------------------------------------------------
 
 classifier <- function(article_text, prompt, chat_object = hf_model){
   
@@ -25,7 +30,6 @@ classifier <- function(article_text, prompt, chat_object = hf_model){
 
   raw_response <- chat_object$chat(
     user_prompt,
-    echo = "none"
   )
   
   list(

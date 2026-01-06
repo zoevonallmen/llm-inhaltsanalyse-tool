cbc1ac3f00ed56eb32e8ea151ebae02f66e6a538
zoevonallmen <z.vonallmen@gmail.com>
2026-01-06 14:49:06 +0100

    continued

--- testing/vitals/vitals_classifier.R
+++ testing/vitals/vitals_classifier.R
@@ -1,6 +1,8 @@
 # Artikel ----------------------------------------------------------------------
-article_short <- readLines("testing/testprompts/article_8.txt")
-article_long #noch auswählen
+article_short <- readLines("testing/testprompts/article_24_SHORT.txt")
+article_long <- readLines("testing/testprompts/article_6_LONG.txt")
+
+article_8 <- readLines("testing/testprompts/article_8.txt") #2766 Zeichen
 
 # Prompts ---------------------------------------------------------------------
 prompt_short <- "Codieren Sie den Text nach den Kriterien 'Wirtschaft', 'Sicherheit', 'Kultur' oder 'Humanitär' und begründen Sie Ihre Wahl."
@@ -10,13 +12,24 @@ prompt_long <- paste(
   collapse = "\n"
 )
 
-# prompt_long_with_examples <- paste(
-#   readLines("testing/testprompts/testprompt_mit Bsp.txt", encoding = "UTF-8"),
-#   collapse = "\n"
-# ) --> evtl. auch noch testen? 
+prompt_long_with_examples <- paste(
+  readLines("testing/testprompts/testprompt_mit Bsp.txt", encoding = "UTF-8"),
+collapse = "\n"
+) #--> evtl. auch noch testen? 
 
-# Test: Artikellänge -----------------------------------------------------------
+# Test: Artikel- und Promptlänge ----------------------------------------------
+source("R/classifier.R")
 
-#Test: Promptlänge -------------------------------------------------------------
+response <- classifier(
+  article_text = article_long,
+  prompt = prompt_long_with_examples,
+  chat_object = hf_model
+)
 
-#Test: Batch -------------------------------------------------------------------
+response$success
+response$code
+response$reasoning
+response$raw
+
+
+# Loop ----------------------------------------------------------------------------
